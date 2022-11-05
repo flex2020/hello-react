d108c089bbc2f72c5a68a26bfb385e8ff491eeca
flex2020 <hosan_1@naver.com>
2022-11-05 19:15:52 +0900

    컴포넌트를 Create하는 컴포넌트생성

--- src/App.js
+++ src/App.js
@@ -58,17 +58,37 @@ function Article(props) {
   )
 }
 
+function Create(props) {
+  return (
+  <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+
+    </form>
+  </article>
+  )
+}
+
 function App() {
   /**
    * state로 내부 상태를 바꿀 수도 있음
    */
   const [mode, setMode] = useState('WELCOME'); // 초기상태 WELCOME으로 만듦. _mode는 배열임 _mode[0] = 상태의 값, _mode[1] = setMode함수
-  const [id, setId] = useState(null); 
-  const topics = [
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4); 
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body: 'html is ...'},
     {id:2, title:'css', body: 'css is ...'},
     {id:3, title:'javascript', body: 'javascript is ...'}
-  ];
+  ]);
   let content = null;
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web!"></Article>;
@@ -82,6 +102,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId ,title:_title, body:_body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>;
   }
   return (
     <div>
@@ -93,6 +123,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href="create" onClick={event => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
