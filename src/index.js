fec382269906e2e2f801c715a72b8681d3be2573
flex2020 <hosan_1@naver.com>
2022-11-05 09:56:27 +0900

    Create Component
    Create Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+//--- 이 부분에는 리액트 앱의 전역적인 설정이 들어감.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // public 폴더의 index.html의 id가 root인 태그를 렌더링함.
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
